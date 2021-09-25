a37d1b25477664f1e66ae9c32600abf3bb969145
Felipe Siqueira de Oliveira <90703489+Felipe-Siqueira@users.noreply.github.com>
2021-09-25 14:53:59 -0300

    Aula- 1
    Foi feito uma calculadora, iniciando na lógica de programação

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-function mudaModo() {
-  document.body.classList.toggle("dark");
+var nome = " Felipe ";
+var nDPB = 9;
+var nDSB = 7;
+var nDTB = 5;
+var nDQB = 4;
+var mediaFinal = (nDPB + nDSB + nDTB + nDQB) / 4;
+var notaFixada = mediaFinal.toFixed(1);
+console.log(" bem vindo" + nome);
+console.log(notaFixada);
+if (mediaFinal >= 7) {
+  console.log("Você passou");
+} else {
+  console.log("Que pena, você foi reprovado");
 }
+//condições para reprovação
+
+// isso é um comentário
+// revisão
+// variáveis, strings (palavras), console.log (imprime), toFixed, operações matemáticas, concatenação no console.log "bem vindo" + nome//
